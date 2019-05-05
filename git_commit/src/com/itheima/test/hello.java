aad6a6f4ee75058f86906e61b30dbbfb5e66a15d
quququshu <1483691523@qq.com>
2019-05-05 19:30:55 +0800

    修改内容

--- git_commit/src/com/itheima/test/hello.java
+++ git_commit/src/com/itheima/test/hello.java
@@ -4,4 +4,8 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("hello".getBytes().length);
     }
+    public void test(){
+        int i =0;
+        i+=10;
+    }
 }
